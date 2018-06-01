00d749e5e6c8a820f14cebaf9b6efd6c78ed3ddf
mirazasxx <mants@deskvibe.co>
2018-06-01 09:38:41 +0200

    Migrating from MCrypt to Openssl encryption library

--- src/Encryption/AesEncryption.php
+++ src/Encryption/AesEncryption.php
@@ -15,15 +15,12 @@ class AesEncryption
     # Properties
     #------------------------------------------------------------------------------------------------------
 
-    /**
-     * @var string
-     */
-    private $secureKey;
+    const CHIPPER = 'AES256';
 
     /**
      * @var string
      */
-    private $iv;
+    private $secureKey;
 
     #------------------------------------------------------------------------------------------------------
     # Magic methods
@@ -37,7 +34,6 @@ class AesEncryption
     public function __construct($aesKey)
     {
         $this->secureKey = hash('sha256', $aesKey, true);
-        $this->iv = mcrypt_create_iv(32);
     }
 
     #------------------------------------------------------------------------------------------------------
@@ -53,7 +49,7 @@ class AesEncryption
     public function encrypt($input)
     {
         return base64_encode(
-            mcrypt_encrypt(MCRYPT_RIJNDAEL_256, $this->secureKey, $input, MCRYPT_MODE_ECB, $this->iv)
+            openssl_encrypt ($input, self::CHIPPER, $this->secureKey)
         );
     }
 
@@ -66,7 +62,7 @@ class AesEncryption
     public function decrypt($input)
     {
         return trim(
-            mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $this->secureKey, base64_decode($input), MCRYPT_MODE_ECB, $this->iv)
+            openssl_decrypt(base64_decode($input), self::CHIPPER, $this->secureKey)
         );
     }
 }
